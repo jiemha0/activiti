aba4bb879b3bcb44afc074f0284d2c5b967578f1
无名小卒 <30365997+jiemha0@users.noreply.github.com>
2017-10-29 17:30:54 +0800

    流程变量

---
+++ HelloActiviti/src/main/java/com/jiem/activiti/variable/AppayBillBean.java
@@ -0,0 +1,39 @@
+package com.jiem.activiti.variable;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class AppayBillBean  implements Serializable{
+	private static final long serialVersionUID = 8616200039620886903L;
+	
+	private Integer id;
+	private Integer cost;//金额
+	private String appayPerson;//申请人
+	private Date date;//申请日期
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getCost() {
+		return cost;
+	}
+	public void setCost(Integer cost) {
+		this.cost = cost;
+	}
+	public String getAppayPerson() {
+		return appayPerson;
+	}
+	public void setAppayPerson(String appayPerson) {
+		this.appayPerson = appayPerson;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+
+}
